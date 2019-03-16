ffc3cf2cfebd833182721c2f1e7ad5a50071eafa
thecodermarcelo <thecodermarcelo@gmail.com>
2019-03-16 11:52:58 -0300

    Bla

--- index.js
+++ index.js
@@ -5,7 +5,4 @@ app.get("*", function(req, res) {
   res.send("Alô criançada o bozo chegou")
 })
 
-let port = 7000
-const server = app.listen(port, function() {
-  console.log(`Running on port ${port}`)
-})
+module.exports = app
